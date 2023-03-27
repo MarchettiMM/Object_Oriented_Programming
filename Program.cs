0c4e5ed85aad1d29b350e3ddd40ba9ce29269266
Matheus Marchetti Melo <matheusmmarchetti@gmail.com>
2023-03-26 22:25:50 -0300

    Add C# Exercise
    Addition of an exercise in C# language.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aulas_e_Exercicios_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
